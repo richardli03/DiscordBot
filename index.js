7fa81e1b4ae92c181ee37304cbc58a9cfcd14ca6
Richard Li <richardli485@gmail.com>
2021-01-19 12:18:52 -0500

    @ing the user
    Added an @ to the user who sends the stupid message and changed the name of the bot to Filter Bot!

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const prefix ='!';
 
 // run node
 client.once('ready',()=>{
-  console.log('Anti-Degen Bot is online!')
+  console.log('Filter Bot is online!')
 })
 
 //code here
 client.on('message', message =>{
-  if(message.content.includes("lib")){
+  // TODO: Needs to reference a text file that can be outputted and appended at any time by moderators. That will be the list of bannable words.
+  if(message.content.toLowerCase().includes("lib")){
       if(message.content.includes("equilibrium")||message.content.includes("library")) return;
-
   else  {
       message.delete();
-      message.channel.send('do not speak');
+      message.channel.send('do not speak '+ `<@${message.author.id}>`);
     }
 }
   if(!message.content.startsWith(prefix) || message.author.bot) return;
@@ -30,6 +30,10 @@ client.on('message', message =>{
   }
   else if(command === 'rules'){
     message.channel.send('https://bit.ly/3nZ3S3v')
+  }
+  else if(command === 'votekick')
+  {
+    //
   }
 
 });
