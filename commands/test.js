9dd5546ad5558a9e21140f89447215a605603779
Richard Li <richardli485@gmail.com>
2021-09-17 15:41:29 -0400

    Committing
    Uncommitted stuff gives me anxiety

--- commands/test.js
+++ commands/test.js
@@ -4,11 +4,29 @@ module.exports =
 	description: 'test',
 	execute(message, args) {
 
+		let reason = message.channel.send(`voting to kick ${mentionedUser.username}`);
+		message.react('🇾')
+			.then(()=>message.react('🇳'));
 
-		let fs = require('fs');
-	const array1 = fs.readFileSync('del.txt').toString('utf-8').split('\n');
-	let n=1;
-	message.channel.send('banned words: ' + array1[n])
+		const filter = (reaction, user) => {
+			return ['🇾', '🇳'].includes(reaction.emoji.name) && user.id === message.author.id;
+		};
 
-}
+		message.awaitReactions(filter, { max: 1, time: 10000, errors: ['time'] })
+			.then(collected => {
+				const reaction = collected.first();
+
+				if (reaction.emoji.name === '🇾') {
+					message.reply('you reacted with a yes.');
+				}
+				else {
+					message.reply('you reacted with a no.');
+				}
+			})
+			.catch(collected => {
+				message.reply('you reacted with neither yes nor no.');
+			});
+
+
+	},
 };
--- commands/votekick.js
+++ commands/votekick.js
@@ -4,13 +4,40 @@ module.exports =
 	description: 'this is a votekick function!',
 	execute(message, args) {
 
-
 		if (!message.mentions.users.first()) {
 			message.delete();
-			message.channel.send('please @ somebody in order to votekick them!');
+			message.reply('please @ somebody in order to votekick them!');
 			return;
 		}
+
 		const mentionedUser = message.mentions.users.first();
-		message.channel.send(`Hi, I recieved your command. This is the user that will be kicked in the future: ${mentionedUser.username}`);
+		let reason = message.channel.send(`voting to kick ${mentionedUser.username}`);
+		message.react('🇾')
+			.then(()=>message.react('🇳'));
+
+		const filter = (reaction, user) => {
+			return ['🇾', '🇳'].includes(reaction.emoji.name) && user.id === message.author.id;
+		};
+
+		message.awaitReactions(filter, { max: 1, time: 10000, errors: ['time'] })
+			.then(collected => {
+				const reaction = collected.first();
+
+				if (reaction.emoji.name === '🇾') {
+					message.reply('you reacted with a yes.');
+				}
+				else {
+					message.reply('you reacted with a no.');
+				}
+			})
+			.catch(collected => {
+				message.reply('you reacted with neither yes nor no.');
+			});
+
+
+
+
+
+
 	},
 };
